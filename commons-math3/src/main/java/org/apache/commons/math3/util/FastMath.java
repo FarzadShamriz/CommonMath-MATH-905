0391a3821476b98ea3c400aaca31c030e8a1e894
Farzad Shamriz <farzadshamriz@outlook.com>
2019-11-19 21:13:18 -0500

    Refactoring:
    1) Change the version of JDK to 1.8 because of using the lambda
    expression
    
    2) extract two methods form cosh and sinh
    
    3) extract a method to call with two extracted methods to resolve
    duplication

--- commons-math3/src/main/java/org/apache/commons/math3/util/FastMath.java
+++ commons-math3/src/main/java/org/apache/commons/math3/util/FastMath.java
@@ -390,13 +390,8 @@ public class FastMath {
       // for numbers with magnitude 20 or so,
       // exp(-z) can be ignored in comparison with exp(z)
 
-      if (x > 20.0) {
-          return exp(x)/2.0;
-      }
-
-      if (x < -20) {
-          return exp(-x)/2.0;
-      }
+      Double processedX = processLargeNumberCosh(x);
+      if(processedX!=null) return processedX;
 
       double hiPrec[] = new double[2];
       if (x < 0.0) {
@@ -450,13 +445,8 @@ public class FastMath {
       // for values of z larger than about 20,
       // exp(-z) can be ignored in comparison with exp(z)
 
-      if (x > 20.0) {
-          return exp(x)/2.0;
-      }
-
-      if (x < -20) {
-          return -exp(-x)/2.0;
-      }
+      Double processedX = processLargeNumberSinh(x);
+      if(processedX!=null) return processedX;
 
       if (x == 0) {
           return x;
@@ -548,6 +538,25 @@ public class FastMath {
       }
 
       return result;
+    }
+
+    private static double processLargeNumberCosh(double x) {
+    	return processLargeNumberHyperbolic(x, () -> exp(-x));
+    }
+    
+    private static double processLargeNumberSinh(double x) {
+    	return processLargeNumberHyperbolic(x, () -> -exp(-x));
+    }
+    
+    private static Double processLargeNumberHyperbolic(double x,Supplier<Double> arg0) {
+    	if (x > 20.0) {
+            return exp(x)/2.0;
+        }
+
+        if (x < -20) {
+            return arg0.get()/2.0;
+        }
+        return null;
     }
     
     /** Compute the hyperbolic tangent of a number.
